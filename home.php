69b3251635ce66426d9cce0a3c0dd97629eebd49
jonathanfrancisco <jonathanfrancisco999@gmail.com>
2018-03-22 01:34:00 +0800

    Add dashboard page

---
+++ home.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home / Dashboard</title>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body class="home">
+
+    <div class="home__header">
+        <h1>Welcome, Bro!</h1>
+        <h2 class="home__header__time">7:30 AM</h2>
+        <a href="logout.php">Logout</a>
+    </div>
+
+    <div class="home__tile">
+        <img src="assets/images/sell.png">
+        <div class="home__tile__description">
+            <h1>Sell Products</h1>
+            <p>Lorem Ipsum bla bla bla</p>
+        </div>  
+    </div>
+    <div class="home__tile">
+        <img src="assets/images/products.png">
+        <div class="home__tile__description">
+            <h1>Products</h1>
+            <p>Lorem Ipsum bla bla bla</p>
+        </div>  
+    </div>
+    <div class="home__tile">
+        <img src="assets/images/logs.png">
+        <div class="home__tile__description">
+            <h1>Sales & Inventory logs</h1>
+            <p>Lorem Ipsum bla bla bla</p>
+        </div>  
+    </div>
+    <div class="home__tile">
+        <img src="assets/images/category.png">
+        <div class="home__tile__description">
+            <h1>Category</h1>
+            <p>Lorem Ipsum bla bla bla</p>
+        </div>  
+    </div>
+    <div class="home__tile">
+        <img src="assets/images/users.png">
+        <div class="home__tile__description">
+            <h1>Account settings & Users</h1>
+            <p>Lorem Ipsum bla bla bla</p>
+        </div>  
+    </div>
+    <div class="home__tile">
+        <img src="assets/images/brand.png">
+        <div class="home__tile__description">
+            <h1>Brand</h1>
+            <p>Lorem Ipsum bla bla bla</p>
+        </div>  
+    </div>
+    
+</body>
+</html>

3c80a9417d9dccb3b9ac8e310dfc54fb1f8a8c2d
jonathanfrancisco <jonathanfrancisco999@gmail.com>
2018-03-22 02:07:15 +0800

    Update description

--- home.php
+++ home.php
@@ -19,7 +19,7 @@
         <img src="assets/images/sell.png">
         <div class="home__tile__description">
             <h1>Sell Products</h1>
-            <p>Lorem Ipsum bla bla bla</p>
+            <p>Sell your products to your clients all over the Philippines!</p>
         </div>  
     </a>
 
@@ -27,35 +27,35 @@
         <img src="assets/images/products.png">
         <div class="home__tile__description">
             <h1>Products</h1>
-            <p>Lorem Ipsum bla bla bla</p>
+            <p>Add, Edit, and Remove product(s) and Manage Stock(s) of your products</p>
         </div>  
     </a>
     <a href="sales-inventory.php" class="home__tile">
         <img src="assets/images/logs.png">
         <div class="home__tile__description">
             <h1>Sales & Inventory Logs</h1>
-            <p>Lorem Ipsum bla bla bla</p>
+            <p>View sales and recent inventory logs</p>
         </div>  
     </a>
     <a href="category.php" class="home__tile">
         <img src="assets/images/category.png">
         <div class="home__tile__description">
             <h1>Category</h1>
-            <p>Lorem Ipsum bla bla bla</p>
+            <p>Manage categories for your products</p>
         </div>  
     </a>
     <a href="settings-users.php" class="home__tile">
         <img src="assets/images/users.png">
         <div class="home__tile__description">
             <h1>My Account Settings & Users</h1>
-            <p>Lorem Ipsum bla bla bla</p>
+            <p>Manage your account settings and users</p>
         </div>  
     </a>
     <a href="brand.php" class="home__tile">
         <img src="assets/images/brand.png">
         <div class="home__tile__description">
             <h1>Brand</h1>
-            <p>Lorem Ipsum bla bla bla</p>
+            <p>Manage brands for your products</p>
         </div>  
     </a>
     

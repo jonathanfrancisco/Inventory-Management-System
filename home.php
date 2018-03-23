c713317ed06153343bffafa6791ee69730833377
jonathanfrancisco <jonathanfrancisco999@gmail.com>
2018-03-23 23:39:24 +0800

    change to 3 columned

--- home.php
+++ home.php
@@ -16,14 +16,6 @@
     </div>
 
     <div class="home__dashboard">
-        <a href="pos.php" class="home__dashboard__tile">
-            <img src="assets/images/sell.png">
-            <div class="home__dashboard__tile__description">
-                <h1>Sell Products</h1>
-                <p>Sell your products to your clients all over the Philippines!</p>
-            </div>  
-        </a>
-
         <a href="products.php" class="home__dashboard__tile">
             <img src="assets/images/products.png">
             <div class="home__dashboard__tile__description">
@@ -31,25 +23,11 @@
                 <p>Add, Edit, and Remove product(s) and Manage Stock(s) of your products</p>
             </div>  
         </a>
-        <a href="sales-inventory.php" class="home__dashboard__tile">
-            <img src="assets/images/logs.png">
-            <div class="home__dashboard__tile__description">
-                <h1>Sales & Inventory Logs</h1>
-                <p>View sales and recent inventory logs</p>
-            </div>  
-        </a>
-        <a href="category.php" class="home__dashboard__tile">
+         <a href="category.php" class="home__dashboard__tile">
             <img src="assets/images/category.png">
             <div class="home__dashboard__tile__description">
                 <h1>Category</h1>
                 <p>Manage categories for your products</p>
-            </div>  
-        </a>
-        <a href="settings-users.php" class="home__dashboard__tile">
-            <img src="assets/images/users.png">
-            <div class="home__dashboard__tile__description">
-                <h1>My Account Settings & Users</h1>
-                <p>Manage your account settings and users</p>
             </div>  
         </a>
         <a href="brand.php" class="home__dashboard__tile">
@@ -59,6 +37,28 @@
                 <p>Manage brands for your products</p>
             </div>  
         </a>
+        <a href="pos.php" class="home__dashboard__tile">
+            <img src="assets/images/sell.png">
+            <div class="home__dashboard__tile__description">
+                <h1>Sell Products</h1>
+                <p>Sell your products to your clients all over the Philippines!</p>
+            </div>  
+        </a>
+        <a href="sales-inventory.php" class="home__dashboard__tile">
+            <img src="assets/images/logs.png">
+            <div class="home__dashboard__tile__description">
+                <h1>Sales & Inventory Logs</h1>
+                <p>View sales and recent inventory logs</p>
+            </div>  
+        </a>
+        <a href="settings-users.php" class="home__dashboard__tile">
+            <img src="assets/images/users.png">
+            <div class="home__dashboard__tile__description">
+                <h1>My Account Settings & Users</h1>
+                <p>Manage your account settings and users</p>
+            </div>  
+        </a>
+       
     </div>
 
     <script src="assets/app.js"></script>

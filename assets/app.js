df0558dd56e291ab6f2bad0e33272fe39c630ae6
jonathanfrancisco <jonathanfrancisco999@gmail.com>
2018-04-18 22:56:12 +0800

    Prevent body from scrolling when a modal is opened

--- assets/app.js
+++ assets/app.js
@@ -16,13 +16,17 @@ setInterval(tickClock,1000);
 function toggleAddModal() {
    document.querySelector('.modal-add').classList.toggle('show-modal');
    document.querySelector('.modal-add__form').classList.toggle('show-modal__form');
+   document.querySelector('body').classList.toggle('modal-body--open');  
 }
 
 function toggleEditModal() {
     document.querySelector('.modal-edit').classList.toggle('show-modal');
+    document.querySelector('body').classList.toggle('modal-body--open');  
 }
 
 
 function toggleStockModal() {
     document.querySelector('.modal-stock').classList.toggle('show-modal');
+    document.querySelector('body').classList.toggle('modal-body--open');  
 }
+
